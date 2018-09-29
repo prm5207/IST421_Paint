342c21ac8d48a881ec2f960fd60d9d8c25d841b2
Patrick Murphy <pmurphy552@gmail.com>
2018-09-29 11:24:10 -0400

    Add files via upload

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,4 +18,17 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
       .pipe(map(res => res.json()));
   }
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user,{headers: headers})
+      .pipe(map(res => res.json()));
+  }
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
 }
