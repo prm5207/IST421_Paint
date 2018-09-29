2b2cada80a993aeaf40349caeb95fac6dd70d8f8
Patrick Murphy <pmurphy552@gmail.com>
2018-09-29 11:21:25 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const passport = require("passport");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
 const config = require('./config/database');
-//DATABASE 
-mongoose.connect(config.database); 
-//Database on Connection
-mongoose.connection.on('connected', function(){
-    console.log('Connected to database '+config.database);
+
+// Connect To Database (NEW) But not working!!!!!!!!!! (because of secret in db.js!!!!!)
+//const db = require('./config/database');
+// Map global promise - get rid of warning
+//mongoose.Promise = global.Promise;
+// Connect to mongoose
+//mongoose.connect(db.mongoURI, {
+    //useMongoClient: true
+//})
+//.then(() => console.log('MongoDB Connected...'))
+//.catch(err => console.log(err));
+
+
+// Connect To Database (OLD CODE)
+mongoose.connect(config.database);
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database '+config.database);
 });
-//Database on Error
-mongoose.connection.on('error', function(err){
-    console.log('Database Error '+err);
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
 });
 
 const app = express();
 
 const users = require('./routes/users');
 
-//PORT number
-const port = 3000;
+// Port Number
+const port = process.env.PORT || 8080;
 
-//CORS Middleware
+// CORS Middleware
 app.use(cors());
 
-//Set static folder
-app.use(express.static(path.join(__dirname,'public')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-//Passport Middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -40,12 +53,16 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-//Index Route
-app.get('/', function(req,res){
-    res.send("Invalid Endpoint");
+// Index Route
+app.get('/', (req, res) => {
+  res.send('invaild endpoint');
 });
 
-//Start Server
-app.listen(port, function(){
-    console.log("Server started on port "+port);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
 });
